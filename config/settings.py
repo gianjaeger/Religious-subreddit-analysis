bf6521bc17d64b669fa850f0f3eed80ce1e28979
Matteo Larrode <matteo.larrode@gmail.com>
2024-11-05 11:56:24 +0000

    more config things

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 # config/settings.py
 # Remember to replace the placeholder values with your own Reddit API credentials.
-USER_AGENT = "SDS_textanalysis/1.0 (by /u/matteolarrode)"
+USER_AGENT = "SDS_textanalysis/1.0 (by /u/group1_3)"
 API_BASE_URL = "https://api.reddit.com"
 RATE_LIMIT_DELAY = 2
